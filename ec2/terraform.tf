d8463c63b1442e015f2a81684f60d0b809bca824
Furkhan <your@email.com>
2025-08-15 22:03:36 +0530

    *

---
+++ ec2/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+  backend "s3" {
+    bucket               = "mirinda-soda"
+    key                  = "terraform.tfstate"
+    region               = "us-east-1"
+    dynamodb_table       = "dynabol"
+    encrypt              = true
+    workspace_key_prefix = "env"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
